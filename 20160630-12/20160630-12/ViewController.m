113a759004a2a65ed3fdee35abad9277ad947dcf
lisi <lisi@qq.com>
2016-06-30 16:25:13 +0800

    第一次修改代码

--- 20160630-12/20160630-12/ViewController.m
+++ 20160630-12/20160630-12/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"爸爸的第一份代码");
 }
 
 - (void)didReceiveMemoryWarning {
